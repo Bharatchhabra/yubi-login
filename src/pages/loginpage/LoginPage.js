888cf95077dd3862813a0e6afe0d01ac90dc4c1b
brinavneet <navneet.brinfotech@gmail.com>
2022-02-01 19:17:54 +0530

    first commit

---
+++ src/pages/loginpage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/layout/header/Header'
+import { Login } from './login/Login'
+
+export const LoginPage = () => {
+    return (
+        <div>
+            <Header />
+            <Login />
+        </div>
+    )
+}
